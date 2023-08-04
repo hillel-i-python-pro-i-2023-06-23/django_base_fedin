742aa6ddab79bffc5e5282359f6a4ad86786cc9b
dafedin <tm.i.sm.master@gmail.com>
2023-08-04 15:10:27 +0300

    Init

---
+++ core/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path, include
+
+
+# Add routes instead of using decorators in views
+urlpatterns = [
+    path("my/", include("apps.my.urls")),
+]
--- manage.py
+++ manage.py
@@ -4,9 +4,11 @@ import os
 import sys
 
 
+# Mane file to run project
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_base_fedin.settings')
+    # Set path to file with main settings
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -18,5 +20,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
