55fa85a68d9829c26f472d74c37c1f5a26279119
dafedin <tm.i.sm.master@gmail.com>
2023-08-06 10:33:28 +0300

    Game model added

--- apps/basic/models.py
+++ apps/basic/models.py
@@ -1,14 +1,6 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class CustomUser(AbstractUser):
-    pass
-
-    # def __str__(self):
-    #     return self.username
-
-
-class MyModel(models.Model):
-    name = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='images/')
+class DataStored(models.Model):
+    stored_string = models.CharField(max_length=20)
+    stored_string1 = models.CharField(max_length=20)
