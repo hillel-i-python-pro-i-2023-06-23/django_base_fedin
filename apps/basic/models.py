8ec061d4fbf8b13db8764cf0f89b82f4533d2974
dafedin <tm.i.sm.master@gmail.com>
2023-08-24 22:45:58 +0300

    Contact model added

--- apps/basic/models.py
+++ apps/basic/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 from django.db import models
 
 
@@ -8,3 +9,38 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+    phone_regex = RegexValidator(
+        regex=r"^\+?1?[-().\dx]{9,20}$",
+        message="Phone number is invalid",
+    )
+    phone_number = models.CharField(
+        validators=[phone_regex], max_length=17, blank=True
+    )
+
+    is_auto_generated = models.BooleanField(
+        blank=False,
+        default=False,
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        blank=False,
+        null=False,
+    )
+
+    modified_at = models.DateTimeField(
+        auto_now=True,
+        blank=False,
+        null=False,
+    )
+
+    def __str__(self):
+        return f"{self.name}"
+
+    # Get metadata from model (sort by newest record)
+    class Meta:
+        ordering = ["-modified_at", "name"]
