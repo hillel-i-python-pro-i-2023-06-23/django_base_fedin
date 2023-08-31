efce14e5071c52904378e75cc55389f3cdc3f300
dafedin <tm.i.sm.master@gmail.com>
2023-08-31 15:12:32 +0300

    Contact data generation added as command

--- apps/contacts/management/commands/generate_contact_data.py
+++ apps/contacts/management/commands/generate_contact_data.py
@@ -1,20 +1,35 @@
+import logging
+
 from django.core.management.base import BaseCommand
 from apps.contacts.models.contact import Contact
 from apps.contacts.models.contact_data import ContactData
 from apps.contacts.models.contact_data_type import ContactDataType
+from apps.contacts.services.faker import fake_contact
 
 
 class Command(BaseCommand):
     help = "Create each type of contact data for each contact"
 
     def handle(self, *args, **options):
-        contact_type_names = ["Phone Number", "LinkedIn Account", ]
+        contact_type_names = ["Phone Number", "LinkedIn Account", "Email"]
 
         contact_types = [ContactDataType.objects.get_or_create(name=name)[0] for name in contact_type_names]
 
+        logger = logging.getLogger("django")
+
         for contact in Contact.objects.all():
+            contact_name = contact.name
+
+            logger.info(f"contact_types: {contact_name}")
+
+            value = None
+
             for contact_type in contact_types:
-                value = "smth"
+                if contact_type.name == "Phone Number":
+                    value = fake_contact.get_phone()
+                elif contact_type.name == "Email":
+                    value = fake_contact.get_email(contact_name)
+                elif contact_type.name == "LinkedIn Account":
+                    raw_value = contact_name
+                    value = raw_value[0].lower() + raw_value[1:]
                 ContactData.objects.create(contact=contact, data_type=contact_type, value=value)
-
-
--- apps/contacts/services/faker.py
+++ apps/contacts/services/faker.py
@@ -1,13 +1,26 @@
-from apps.basic.services.faker import FakeUser
+import string
+
+from faker import Faker
 
 
-class FakeContact(FakeUser):
+class FakeContact():
+    def __init__(self):
+        self.faker = Faker()
 
     def get_name(self):
         return self.faker.unique.first_name()
 
     def get_phone(self):
         return self.faker.phone_number()
+
+    def get_domain(self):
+        return self.faker.unique.domain_name()
+
+    def get_email(self, name):
+        email_prefix = name
+        domain = self.get_domain()
+
+        return f"{email_prefix}@{domain}"
 
 
 fake_contact = FakeContact()
