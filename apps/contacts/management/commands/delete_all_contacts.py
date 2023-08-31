2db65fbb2c80dde591dc342ccd549b3170c88983
dafedin <tm.i.sm.master@gmail.com>
2023-08-31 17:01:01 +0300

    Command sequence added

---
+++ apps/contacts/management/commands/delete_all_contacts.py
@@ -0,0 +1,28 @@
+import logging
+
+from django.core.management.base import BaseCommand
+
+from apps.contacts.models.contact import Contact
+
+
+class Command(BaseCommand):
+    help = "Delete contacts"
+
+    def handle(self, *args, **options) -> None:
+
+        # Log handling to terminal
+        logger = logging.getLogger("django")
+
+        # Get queryset template
+        queryset = Contact.objects.all()
+
+        logger.info(f"Current amount of contacts before: {queryset.count()}")
+
+        queryset_for_delete = queryset
+        logger.info("Delete all generated contacts")
+        queryset_for_delete = queryset_for_delete.filter(is_auto_generated=True)
+
+        total_deleted, details = queryset_for_delete.delete()
+        logger.info(f"Total deleted: {total_deleted}, details: {details}")
+
+        logger.info(f"Current amount of contacts after deletion: {queryset.count()}")
--- apps/contacts/management/commands/init_contact_data.py
+++ apps/contacts/management/commands/init_contact_data.py
@@ -4,6 +4,8 @@ from typing import Final
 from django.core.management.base import BaseCommand
 
 from apps.contacts.models.contact import Contact
+from apps.contacts.models.contact_data import ContactData
+from apps.contacts.models.contact_data_type import ContactDataType
 from apps.contacts.services.faker import fake_contact
 
 
@@ -15,11 +17,23 @@ class Command(BaseCommand):
         logger = logging.getLogger("django")
         current_amount_of_contacts = Contact.objects.count()
 
-        if (amount := (self.MINIMAL_AMOUNT_OF_CONTACTS - current_amount_of_contacts)) > 0:
+        amount = (self.MINIMAL_AMOUNT_OF_CONTACTS - current_amount_of_contacts)
+
+        if amount > 0:
             logger.info(f"Current amount of contacts: {Contact.objects.count()} but {amount} needed")
             for _ in range(amount):
-                Contact.objects.create(name=fake_contact.get_name()).save()
+                contact = Contact.objects.create(name=fake_contact.get_name(),is_auto_generated=1).save()
+                # ContactData.objects.create(contact=contact, data_type=ContactDataType.data_type).save()
         else:
             logger.info(f"Current amount of data is enough.")
+
+        logger.info(f"{amount} contacts created")
+
+        # if amount:
+        #     logger.info(f"{amount} contact data needed")
+        #     for _ in range(amount):
+        #         ContactData.objects.create().save()
+        # else:
+        #     logger.info(f"Current amount of data is enough.")
 
         logger.info(f"Final amount of contacts with contact data: {Contact.objects.count()}")
