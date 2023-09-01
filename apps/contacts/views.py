1f5cc8a6fefae842cb4f6a80b445f31c0b2ad818
dafedin <tm.i.sm.master@gmail.com>
2023-09-01 16:40:56 +0300

    Template to list added /contacts/list/

--- apps/contacts/views.py
+++ apps/contacts/views.py
@@ -1,14 +1,28 @@
 from django.shortcuts import render
 from apps.contacts.models.contact import Contact
+from apps.contacts.models.contact_data import ContactData
+
+
+# def contact_home_view(request):
+#     contacts = Contact.objects.all()
+#
+#     return render(
+#         request=request,
+#         template_name='home.html',
+#         context={'contacts': contacts}
+#     )
 
 
 def contact_list_view(request):
     contacts = Contact.objects.all()
+    contac_tdata = ContactData.objects.all()
 
     return render(
         request=request,
         template_name='contact_list.html',
-        context={'contacts': contacts}
+        context={'contacts': contacts,
+                 'contact_data': contac_tdata,
+                 }
     )
 
 
