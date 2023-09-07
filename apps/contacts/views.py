65b44d0b8c3fa502dbc6b22af077d680268554f4
dafedin <tm.i.sm.master@gmail.com>
2023-09-07 13:58:47 +0300

    Contact create subform works well

--- apps/contacts/views.py
+++ apps/contacts/views.py
@@ -24,31 +24,39 @@ def contact_list_view(request):
 
 def create_contact_view(request):
 
-    ContactDataFormSet = formset_factory(ContactDataForm, extra=3)
+    contact_data_form_set = formset_factory(ContactDataForm, extra=3)
 
     if request.method == 'POST':
         contact_form = ContactForm(request.POST)
-        contact_data_formset = ContactDataFormSet(request.POST)
 
-        if contact_form.is_valid() and contact_data_formset.is_valid():
-            contact = contact_form.save()
-            for form in contact_data_formset:
-                if form.is_valid():
-                    data_type = form.cleaned_data['data_type']
-                    value = form.cleaned_data['value']
-
-                    if data_type and value:
-                        contact_data = ContactData.objects.create(contact=contact, data_type=data_type, value=value)
-                        contact_data.save()
-
-            return redirect('contact_list')
+        if contact_form.is_valid():
+            if 'cancel' in request.POST:
+                return redirect('contacts:contact_list')  # Redirect to the other template
+            else:
+                contact_form.save()
+                return redirect('contacts:contact_list')
+    #     contact_data_formset = contact_data_form_set(request.POST)
+    #
+    #     # if contact_form.is_valid() and contact_data_formset.is_valid():
+    #     contact = contact_form.save()
+    #     for form in contact_data_formset:
+    #         if form.is_valid():
+    #             data_type = form.cleaned_data['data_type']
+    #             value = form.cleaned_data['value']
+    #
+    #             if data_type and value:
+    #                 contact_data = ContactData.objects.create(contact=contact, data_type=data_type, value=value)
+    #                 contact_data.save()
+    #
+    #         return redirect('contact_list')
     else:
+
         contact_form = ContactForm()
-        contact_data_formset = ContactDataFormSet()
+    #     contact_data_formset = contact_data_form_set()
 
     return render(
-        request,
-        'create_contact.html',
-        {'contact_form': contact_form,
-         'contact_data_formset': contact_data_formset}
-    )
+        request=request,
+        template_name='create_contact.html',
+        context={'contact_form': contact_form})
+    #      'contact_data_formset': contact_data_formset}
+    # )
