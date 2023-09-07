1d7749f572b860e1cb661a4774f70475614bc8ad
dafedin <tm.i.sm.master@gmail.com>
2023-09-07 22:42:16 +0300

    Separate urls for views

--- apps/contacts/urls.py
+++ apps/contacts/urls.py
@@ -6,5 +6,5 @@ app_name = "contacts"
 urlpatterns = [
     path('list/', views.contact_list_view, name='contact_list'),
     path('create_contact/', views.create_contact_view, name='create_contact'),
-    # path('create_contact/', views.create_contact_data_view, name='create_contact_data'),
+    path('create_contact/', views.create_contact_data_view, name='create_contact_data'),
 ]
