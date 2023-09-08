d3f191bf7763e5720479f9b266cec768e3132fe5
dafedin <tm.i.sm.master@gmail.com>
2023-09-08 14:34:00 +0300

    2 forms in one view done but models connected wrong

--- apps/contacts/views.py
+++ apps/contacts/views.py
@@ -25,6 +25,7 @@ def contact_list_view(request):
 def create_contact_view(request):
     if request.method == "POST":
         contact_form = ContactForm(request.POST)
+        contact_data_form = ContactDataForm(request.POST)
 
         if contact_form.is_valid():
             if "save_contact" in request.POST:
@@ -32,24 +33,14 @@ def create_contact_view(request):
             if "cancel" in request.POST:
                 return redirect("contacts:contact_list")
 
-    else:
-        contact_form = ContactForm()
-
-    return render(request=request,
-                  template_name="create_contact.html",
-                  context={"contact_form": contact_form})
-
-
-def create_contact_data_view(request):
-    if request.method == "POST":
-        contact_data_form = ContactDataForm(request.POST)
-
         if contact_data_form.is_valid():
             if "submit_data" in request.POST:
                 data_type = contact_data_form.cleaned_data["data_type"]
                 value = contact_data_form.cleaned_data["value"]
                 contact = Contact.objects.last()
-                contact_data = ContactData.objects.create(contact=contact, data_type=data_type, value=value)
+                contact_data = ContactData.objects.create(contact=contact,
+                                                          data_type=data_type,
+                                                          value=value)
 
                 contact_data.save()
 
@@ -57,8 +48,34 @@ def create_contact_data_view(request):
                 return redirect("contacts:contact_list")
 
     else:
+        contact_form = ContactForm()
         contact_data_form = ContactDataForm()
 
     return render(request=request,
                   template_name="create_contact.html",
-                  context={"contact_data_form": contact_data_form})
+                  context={"contact_form": contact_form,
+                           "contact_data_form": contact_data_form})
+
+
+# def create_contact_data_view(request):
+#     if request.method == "POST":
+#         contact_data_form = ContactDataForm(request.POST)
+#
+#         if contact_data_form.is_valid():
+#             if "submit_data" in request.POST:
+#                 data_type = contact_data_form.cleaned_data["data_type"]
+#                 value = contact_data_form.cleaned_data["value"]
+#                 contact = Contact.objects.last()
+#                 contact_data = ContactData.objects.create(contact=contact, data_type=data_type, value=value)
+#
+#                 contact_data.save()
+#
+#             if "cancel_data" in request.POST:
+#                 return redirect("contacts:contact_list")
+#
+#     else:
+#         contact_data_form = ContactDataForm()
+#
+#     return render(request=request,
+#                   template_name="create_contact.html",
+#                   context={"contact_data_form": contact_data_form})
